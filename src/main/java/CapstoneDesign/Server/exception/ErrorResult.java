0960d23ff125317eaa3bb95effede123ed353268
anzun0 <zxzimin@naver.com>
2021-10-01 03:27:03 +0900

    add: Exception 발생 시 반환할 객체 생성

---
+++ src/main/java/CapstoneDesign/Server/exception/ErrorResult.java
@@ -0,0 +1,12 @@
+package CapstoneDesign.Server.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private HttpStatus code;
+    private String message;
+}
