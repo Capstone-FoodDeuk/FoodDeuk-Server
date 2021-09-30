e61d3196c5c6aabba798c34fd9b06cd025c64fb5
anzun0 <zxzimin@naver.com>
2021-10-01 04:00:02 +0900

    add: BadRequestException 예외 처리 @ExceptionHandler 추가

--- src/main/java/CapstoneDesign/Server/exception/ExceptionControllerAdvice.java
+++ src/main/java/CapstoneDesign/Server/exception/ExceptionControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
@@ -12,9 +13,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler({MethodArgumentNotValidException.class})
-    public ResponseEntity<ErrorResult> validException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ErrorResult> validExceptionHandle(MethodArgumentNotValidException ex) {
+        log.error("[exceptionHandle] ex", ex);
         ErrorResult errorResult = new ErrorResult(HttpStatus.BAD_REQUEST,
                 "유효성 검사 실패 : " + ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST); // 2
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<ErrorResult> BadRequestExceptionHandle(RuntimeException ex) {
+        log.error("[exceptionHandle] ex", ex);
+        ErrorResult errorResult = new ErrorResult(HttpStatus.BAD_REQUEST, ex.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ src/main/java/CapstoneDesign/Server/exception/LoginIdDuplicatedException.java
@@ -0,0 +1,2 @@
+package CapstoneDesign.Server.exception;public class LoginIdDuplicatedException {
+}
