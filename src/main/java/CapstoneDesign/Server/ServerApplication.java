5e859ec2f0a5e079be41576b006a5efdbfd92f2a
anzun0 <zxzimin@naver.com>
2021-10-01 00:38:55 +0900

    add: querydsl setting

--- src/main/java/CapstoneDesign/Server/ServerApplication.java
+++ src/main/java/CapstoneDesign/Server/ServerApplication.java
@@ -1,13 +1,23 @@
 package CapstoneDesign.Server;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
+	}
 }
