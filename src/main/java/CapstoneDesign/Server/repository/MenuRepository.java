9267b5324075b7c00c4c36463b66d9c79a000667
anzun0 <zxzimin@naver.com>
2021-10-01 13:07:20 +0900

    add: create MenuRepository

---
+++ src/main/java/CapstoneDesign/Server/repository/MenuRepository.java
@@ -0,0 +1,7 @@
+package CapstoneDesign.Server.repository;
+
+import CapstoneDesign.Server.domain.entity.store.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+}
