4fd33dc5b796e0dc917b3930bb2f4401235f76e2
anzun0 <zxzimin@naver.com>
2021-10-01 17:16:58 +0900

    add: create ZzimRepository

---
+++ src/main/java/CapstoneDesign/Server/repository/ZzimRepository.java
@@ -0,0 +1,7 @@
+package CapstoneDesign.Server.repository;
+
+import CapstoneDesign.Server.domain.entity.Zzim;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZzimRepository extends JpaRepository<Zzim, Long> {
+}
